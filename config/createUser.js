5ea497a086b594125891ea2472d0de9e0f052a4e
Jlouisa <jonathan.louisa@gmail.com>
2023-11-07 03:45:46 +0100

    Add post API operation for users

---
+++ config/createUser.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const UserCollection = require("../models/userModel");
+
+const createUser = async (username, password, isAdmin, isSuspended) => {
+  try {
+    const hashedPassword = await bcrypt.hash(password, +process.env.HASH_NUM);
+    const user = new UserCollection({
+      username,
+      password: hashedPassword,
+      isAdmin,
+      isSuspended,
+    });
+    await user.save();
+    console.log(`User created: ${username}`);
+  } catch (err) {
+    console.error(`Error creating user: ${username}`, err);
+  }
+};
+
+module.exports = { createUser };
--- controllers/apiControllerPost.js
+++ controllers/apiControllerPost.js
@@ -1,22 +1,74 @@
 const asyncHandler = require("express-async-handler");
-const mockDB = require("../database/mockDB");
+const { body, validationResult } = require("express-validator");
+const mongoose = require("mongoose");
+const UserCollection = require("../models/userModel");
+const BlogCollection = require("../models/blogModel");
+const CommentCollection = require("../models/commentModel");
+const { createUser } = require("../config/createUser");
+const bcrypt = require("bcryptjs");
 
 // Init fake DB
+const mockDB = require("../database/mockDB");
 const db = mockDB.createMockDB();
 
 //! Create user
-exports.apiUserPost = asyncHandler(async function (req, res, next) {
-  // Create new user
-  const newUser = {
-    username: req.body.username,
-    password: req.body.password,
-    isAdmin: false,
-    isSuspended: false,
-  };
-  // Add to database
-  db.users.push(newUser);
-  res.status(201).json({ message: "User succesfully created", user: newUser, "all users": db.users });
-});
+exports.apiUserPost = [
+  // Validate and sanitize the name field.
+  body("username")
+    .notEmpty()
+    .withMessage("Username must not be empty")
+    .trim()
+    .isLength({ min: 5, max: 50 })
+    .withMessage("Username must be between 5 and 50 characters")
+    .matches(/^[a-zA-Z0-9_]*$/)
+    .withMessage("Username can only contain letters, numbers, and underscores")
+    .custom(async (value) => {
+      // Check if the username is in database
+      const user = await UserCollection.findOne({ username: { $regex: new RegExp(`^${value}$`, "i") } });
+      // if (user || reservedUsernames.includes(value.toLowerCase())) {
+      if (user) {
+        throw new Error();
+      }
+    })
+    .withMessage("Username is unavailable. Please try another one")
+    .escape(),
+  body("passwordConfirm")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Passwords doesn't match"),
+  body("password")
+    .notEmpty()
+    .withMessage("Password must not be empty")
+    .trim()
+    // .matches(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).{3,20}$/)
+    // .withMessage(
+    //   "Password must include at least one letter, one digit, one special character, and be between 3 and 20 characters"
+    // )
+    .escape(),
+  // Process request after validation and sanitization.
+  asyncHandler(async function (req, res, next) {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.status(400).json({
+        data: {
+          input: {
+            username: req.body.username,
+          },
+          errors: errors.array(),
+        },
+      });
+      return;
+    } else {
+      console.log("Validation succesful");
+      createUser(req.body.username, req.body.password, false, false).catch((err) => console.log(err));
+      res.status(201).json({ message: "User succesfully created" });
+    }
+  }),
+];
 
 //! Create blog
 exports.apiBlogPost = asyncHandler(async function (req, res, next) {
